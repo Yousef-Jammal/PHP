b84acd26891824ef6ba6df99a21f8921217b1a88
Yousef-Jammal <ywsfalsaydh@gmail.com>
2024-07-09 15:04:05 +0300

    Delete back_end directory

--- back_end/taskTwo/taskFunction.php
+++
@@ -1,7 +0,0 @@
-//test Two
-    // $word = "1234";
-    // $reversWordList = [];
-    // for($i = strlen($word) ; $i>0; $i--){
-    //     array_push($reversWord[$i]);
-    // }
-    // echo $reversWord;
